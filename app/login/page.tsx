d75ae2b120d52a6372e2c06d6b4b699dd2bc4002
gabrieltacosta <gabriel.tuka@gmail.com>
2025-12-09 21:02:04 -0300

    update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,32 +1,32 @@
-import LoginForm from "@/components/login-form"
-import Link from "next/link"
+import LoginForm from "@/components/login-form";
+import Link from "next/link";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-const ConsultaCpf= async () => {
+const ConsultaCpf = async () => {
   const session = await (await cookies()).get("access_token");
 
   if (session) {
     redirect("/cpf");
   }
-    return (
-        <div className="flex flex-col justify-center items-center gap-10 pb-10">
+  return (
+    <div className="flex flex-col justify-center items-center gap-10 p-6 mb-20">
       <h2 className="font-bold text-3xl">Consulta telefones por CPF.</h2>
       <div>
-        <Button asChild variant={"default"} className="text-black font-medium hover:font-bold bg-[#FFFE03] hover:bg-[#FFFE03]">
-          <Link
-            href="/"
-          >
-            Voltar
-          </Link>
+        <Button
+          asChild
+          variant={"default"}
+          className="text-black font-medium hover:font-bold bg-[#FFFE03] hover:bg-[#FFFE03]"
+        >
+          <Link href="/">Voltar</Link>
         </Button>
       </div>
       <div className="flex max-w-7xl w-full p-5">
         <LoginForm />
       </div>
     </div>
-    );
-}
+  );
+};
 
-export default ConsultaCpf
+export default ConsultaCpf;
